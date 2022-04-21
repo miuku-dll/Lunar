c165dfbf7ae074a5ca16d7efa954038a1343898e
sute <80363046+xSute@users.noreply.github.com>
2022-04-22 00:10:38 +0300

    Create Init.lua

---
+++ Init.lua
@@ -0,0 +1,11 @@
+local Config = ... or {}
+
+local rbxmSuite = loadstring(
+    game:HttpGetAsync("https://github.com/richie0866/rbxm-suite/releases/download/v2.0.3/rbxm-suite.lua")
+)()
+
+local Project = rbxmSuite.launch(rbxmSuite.download("Swift-Softworks/Lunar@latest", "Lunar.rbxm"), {
+    debug = Config.Debug or true
+}) 
+
+rbxmSuite.require(Project.InitUI)(Config.Toggle or Enum.KeyCode.BackSlash)
