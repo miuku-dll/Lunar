c3ada06a2a1cce1b0ec5c21ccbf6efedacfe45d4
sute <80363046+xSute@users.noreply.github.com>
2022-04-22 00:15:02 +0300

    Update Init.lua

--- Init.lua
+++ Init.lua
@@ -4,7 +4,7 @@ local rbxmSuite = loadstring(
     game:HttpGetAsync("https://github.com/richie0866/rbxm-suite/releases/download/v2.0.3/rbxm-suite.lua")
 )()
 
-local Project = rbxmSuite.launch(rbxmSuite.download("Swift-Softworks/Lunar@latest", "Lunar.rbxm"), {
+local Project = rbxmSuite.launch(rbxmSuite.download("xSute/Lunar@latest", "Lunar.rbxm"), {
     debug = Config.Debug or true
 }) 
 
